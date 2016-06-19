c91a0137327b9b35257b7b4a13d108d77df0dff8
raxkin <albertrazquin@hotmail.com>
2016-06-19 20:34:42 +0200

    Finshed first version of tree script

--- Assets/Resources/Scripts/Tree/TreeManager.cs
+++ Assets/Resources/Scripts/Tree/TreeManager.cs
@@ -3,16 +3,16 @@ using System.Collections;
 
 public class TreeManager : MonoBehaviour {
 
-    public int outputSpeed;
-    public int sourceQuantity;
+    public float outputSpeed;
+    public float sourceQuantity;
 
-    private int currentSourceQuantity;
+    private float currentSourceQuantity;
     private bool isWorking;
 
     // Use this for initialization
     void Start () {
-        outputSpeed = 5;
-        sourceQuantity = 50;
+        outputSpeed = 5f;
+        sourceQuantity = 50f;
 
         isWorking = false;
         currentSourceQuantity = sourceQuantity;
@@ -39,6 +39,18 @@ public class TreeManager : MonoBehaviour {
 
     private void work()
     {
-        currentSourceQuantity -= outputSpeed * Time;
+        Debug.Log("Working.");
+        currentSourceQuantity -= outputSpeed * Time.deltaTime;
+        Debug.Log("Remaining source quantity: " + currentSourceQuantity + ".");
+        if(currentSourceQuantity <= 0)
+        {
+            endWork();
+        }
+    }
+
+    private void endWork()
+    {
+        Debug.Log("End Work.");
+        isWorking = false;
     }
 }
