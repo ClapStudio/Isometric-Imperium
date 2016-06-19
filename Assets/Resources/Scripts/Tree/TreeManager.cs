6298134c41003447a5b12a8bd264be51bf05ee1c
raxkin <albertrazquin@hotmail.com>
2016-06-19 20:14:49 +0200

    Created tree script and prototype1 scene

---
+++ Assets/Resources/Scripts/Tree/TreeManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeManager : MonoBehaviour {
+
+    public int outputSpeed;
+    public int sourceQuantity;
+
+    private int currentSourceQuantity;
+    private bool isWorking;
+
+    // Use this for initialization
+    void Start () {
+        outputSpeed = 5;
+        sourceQuantity = 50;
+
+        isWorking = false;
+        currentSourceQuantity = sourceQuantity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startWork();
+        }
+
+	    if(isWorking)
+        {
+            work();
+        }
+	}
+
+    private void startWork()
+    {
+        Debug.Log("Start Work.");
+        isWorking = true;
+    }
+
+    private void work()
+    {
+
+    }
+}
